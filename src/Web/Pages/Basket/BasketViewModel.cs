9913409207f731c0776c43e62ca10fec42bca5e3
dyigitbas <dyigitbas@microsoft.com>
2023-04-11 09:59:50 -0600

    demo ready v1

--- src/Web/Pages/Basket/BasketViewModel.cs
+++ src/Web/Pages/Basket/BasketViewModel.cs
@@ -6,17 +6,8 @@ public class BasketViewModel
     public List<BasketItemViewModel> Items { get; set; } = new List<BasketItemViewModel>();
     public string? BuyerId { get; set; }
 
-    public bool WasPromotionApplied { get; set; }
-
     public decimal Total()
     {
-        var total = Math.Round(Items.Sum(x => x.UnitPrice * x.Quantity), 2);
-        if (total > 55)
-        {
-            this.WasPromotionApplied = true;
-            total = Math.Round(total * new decimal(0.9), 2); // this line should be missing in the bug
-        }
-
-        return total;
+        return Math.Round(Items.Sum(x => x.UnitPrice * x.Quantity), 2);
     }
 }
