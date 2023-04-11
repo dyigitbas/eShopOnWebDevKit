8bf70b3e08bd69319c5b1a7428c5e3ec6cacc2c6
dyigitbas <dyigitbas@microsoft.com>
2023-04-11 09:31:30 -0600

    first commit - all working w/ all files

---
+++ src/Web/Pages/Basket/BasketViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.eShopWeb.Web.Pages.Basket;
+
+public class BasketViewModel
+{
+    public int Id { get; set; }
+    public List<BasketItemViewModel> Items { get; set; } = new List<BasketItemViewModel>();
+    public string? BuyerId { get; set; }
+
+    public bool WasPromotionApplied { get; set; }
+
+    public decimal Total()
+    {
+        var total = Math.Round(Items.Sum(x => x.UnitPrice * x.Quantity), 2);
+        if (total > 55)
+        {
+            this.WasPromotionApplied = true;
+            total = Math.Round(total * new decimal(0.9), 2); // this line should be missing in the bug
+        }
+
+        return total;
+    }
+}
